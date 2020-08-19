0a8fe41c537d3e0a4b50e9a421954a77cf835523
Mahdi Ahmadi <mahdi.ahmadi.py@gmail.com>
2020-08-19 13:00:31 +0430

    13:00  19/8/2020

--- accounts/views.py
+++ accounts/views.py
@@ -3,5 +3,10 @@ from django.contrib.auth.forms import UserCreationForm
 
 
 def signup_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # login
     form = UserCreationForm()
     return render(request,'accounts/signup.html',{'form':form})
